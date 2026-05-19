b3f1dfd4cc207424fc12ef5dc4786a9ed8e79a34
Baivab Sarkar <baivabsarkar@gmail.com>
2026-05-19 17:12:48 +0530

    Update: Action/Alerts Testing Automation

--- Selenium_Code/src/WebDriver_Pack/ATPBlogPost.java
+++ Selenium_Code/src/WebDriver_Pack/ATPBlogPost.java
@@ -1,18 +1,29 @@
 package WebDriver_Pack;
 
+import java.time.Duration;
+import java.util.Set;
+
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ATPBlogPost {
+
 	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver = new ChromeDriver();
-		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		driver.manage().window().maximize();
 		driver.get("https://testautomationpractice.blogspot.com/");
-		
+
+/*
+		//Day 1
 		WebElement name = driver.findElement(By.id("name"));
 		name.sendKeys("Hello Hola");
 		
@@ -72,15 +83,122 @@ public class ATPBlogPost {
 		WebElement muplode = driver.findElement(By.id("multipleFilesInput"));
 		muplode.sendKeys("C:\\Users\\User\\Downloads\\latex_test_suite.md");
 		muplode.sendKeys("C:\\Users\\User\\Downloads\\mermaid_test_suite.md");
+*/
 		
+		// Day2
+
+		JavascriptExecutor script = (JavascriptExecutor)driver;
+		script.executeScript("window.scrollBy(0,-500)");
+		Thread.sleep(2000);
 		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(8));
 		
+		driver.findElement(By.id("alertBtn")).click();
+		Thread.sleep(1000);
 		
+		wait.until(ExpectedConditions.alertIsPresent());
+		driver.switchTo().alert().accept();
+		Thread.sleep(1000);
 		
+		driver.findElement(By.id("confirmBtn")).click();
+		Thread.sleep(1000);
 		
-		Thread.sleep(5000);
+		wait.until(ExpectedConditions.alertIsPresent());
+		driver.switchTo().alert().accept();
+		Thread.sleep(1000);
+		
+		driver.findElement(By.id("promptBtn")).click();
+		Thread.sleep(1000);
+		
+		wait.until(ExpectedConditions.alertIsPresent());
+		Alert promptAlert = driver.switchTo().alert();
+		promptAlert.sendKeys("Harsh");
+		Thread.sleep(1000);
+		promptAlert.accept();
+		Thread.sleep(1000);
+		
+		String mainWindowHandle = driver.getWindowHandle();
+		WebElement newTabBtn = driver.findElement(By.xpath("//button[normalize-space()='New Tab']")); 
+		newTabBtn.click();
+		Thread.sleep(2000);
+
+		Set<String> allHandles = driver.getWindowHandles();
+
+		for (String handle : allHandles) {
+		    if (!handle.equals(mainWindowHandle)) {
+		        driver.switchTo().window(handle);
+		        System.out.println("Switched to New Tab! URL: " + driver.getCurrentUrl());
+		        Thread.sleep(2000);
+		        
+		        driver.close();
+		        System.out.println("New Tab Closed!");
+		        break;
+		    }
+		}
+
+		driver.switchTo().window(mainWindowHandle);
+		Thread.sleep(1000);
+		
+		WebElement popupBtn = driver.findElement(By.xpath("//*[@id=\"PopUp\"]"));
+		popupBtn.click();
+		Thread.sleep(2000);
+		
+		allHandles = driver.getWindowHandles();
+
+		for (String handle : allHandles) {
+		    if (!handle.equals(mainWindowHandle)) {
+		        driver.switchTo().window(handle);
+		        System.out.println("Switched to Popup Window! Title: " + driver.getTitle());
+		        driver.manage().window().maximize();
+		        Thread.sleep(2000);
+	
+		        driver.close();
+		        System.out.println("Popup Window Closed!");
+		        break;
+		    }
+		}
+		
+		driver.switchTo().window(mainWindowHandle);
+		System.out.println("Back to Main Window.");
+		
+		script.executeScript("window.scrollBy(0,400)");
+		Thread.sleep(2000);
+		
+		Actions action = new Actions(driver);
+		WebElement pointMe = driver.findElement(By.cssSelector(".dropdown>.dropbtn"));
+		
+		action.moveToElement(pointMe).perform();
+		Thread.sleep(1000);
+		
+		WebElement copyText = driver.findElement(By.xpath("//*[@id=\"HTML10\"]/div[1]/button"));		
+		action.doubleClick(copyText).perform();
+		
+		script.executeScript("window.scrollBy(0,500)");
+		Thread.sleep(2000);
+		
+		WebElement sourceBox = driver.findElement(By.id("draggable"));
+		WebElement targetBox = driver.findElement(By.id("droppable"));	
+		action.dragAndDrop(sourceBox, targetBox).perform();
+		Thread.sleep(2000);
+		
+		WebElement sliderLeft = driver.findElement(By.xpath("//*[@id=\"slider-range\"]/span[1]"));		
+		action.dragAndDropBy(sliderLeft, 30, 0).perform();
+		
+		WebElement sliderRight = driver.findElement(By.xpath("//*[@id=\"slider-range\"]/span[2]"));		
+		action.dragAndDropBy(sliderRight, -20, 0).perform();		
+
+		script.executeScript("window.scrollBy(0,400)");
+		Thread.sleep(2000);
+		
+		driver.findElement(By.id("comboBox")).click();
+		Thread.sleep(1000);
+
+		WebElement targetItem = driver.findElement(By.xpath("//div[text()='Item 5']"));
+		targetItem.click();		
+		
+		Thread.sleep(4000);
 		driver.quit();
-		
+
 	}
 
 }
