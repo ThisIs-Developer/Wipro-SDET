75531340c33b50b598e3d7036e89481deb3d51bb
Baivab Sarkar <baivabsarkar@gmail.com>
2026-06-01 16:33:03 +0530

    Update: random email generate

--- ProjectTutorialsNinjaBBD/src/test/java/stepDefinitions/TutorialsNinjaSteps.java
+++ ProjectTutorialsNinjaBBD/src/test/java/stepDefinitions/TutorialsNinjaSteps.java
@@ -1,18 +1,25 @@
 package stepDefinitions;
 
 import java.io.FileOutputStream;
+import java.util.Random;
+
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import io.cucumber.java.en.*;
 
-import pages.TutorialsNinjaPage;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 import hooks.Hooks;
+import pages.TutorialsNinjaPage;
 
 public class TutorialsNinjaSteps {
-    
+
     static TutorialsNinjaPage page;
-    String excelPath = "C:\\Users\\User\\Desktop\\SDET_Wipro\\ProjectTutorialsNinjaBBD\\TestData\\TutorialsNinja_RegisterUserData.xlsx";
+
+    String excelPath =
+            "TestData/TutorialsNinja_RegisterUserData.xlsx";
 
     @Given("user opens TutorialsNinja website")
     public void openWebsite() {
@@ -20,20 +27,28 @@ public class TutorialsNinjaSteps {
         if (!currentUrl.contains("tutorialsninja.com")) {
             Hooks.driver.get("https://tutorialsninja.com/demo/");
         }
-        if(page == null) {
+        if (page == null) {
             page = new TutorialsNinjaPage(Hooks.driver);
         }
     }
 
+    private String generateRandomEmail() {
+        long timestamp = System.currentTimeMillis();
+        int randomNum = new Random().nextInt(10000);
+        return "tester.bot_" + timestamp + "_" + randomNum + "@automation.com";
+    }
+
     @When("user registers with valid details")
     public void registerUser() throws Exception {
+
         String fname = "Test";
         String lname = "Selenium";
-        String email = "testselenium983625@gmail.com";
+        String email = generateRandomEmail();
         String phone = "9876543210";
         String pass = "test@123";
         String confirmPass = "test@123";
 
+        System.out.println("Generated Email: " + email);
         XSSFWorkbook workbook = new XSSFWorkbook();
         XSSFSheet sheet = workbook.createSheet("RegisterData");
         XSSFRow header = sheet.createRow(0);
@@ -54,76 +69,96 @@ public class TutorialsNinjaSteps {
         dataRow.createCell(4).setCellValue(pass);
         dataRow.createCell(5).setCellValue(confirmPass);
 
-        FileOutputStream fos = new FileOutputStream(excelPath);
-        workbook.write(fos);
-        fos.close();
+        try (FileOutputStream fos = new FileOutputStream(excelPath)) {
+            workbook.write(fos);
+        }
+
         workbook.close();
-        
-        page.register(fname,lname,email,phone,pass,confirmPass);
+
+        page.register(
+                fname,
+                lname,
+                email,
+                phone,
+                pass,
+                confirmPass);
     }
 
     @Then("registration should be successful")
     public void registrationSuccess() {
+
         System.out.println("Registration Successful");
     }
 
     @When("user logs in using excel data")
     public void loginUser() throws Exception {
+
         page.loginFromExcel(excelPath);
     }
 
     @Then("login should be successful")
     public void loginSuccess() {
+
         System.out.println("Login Successful");
     }
 
     @When("user searches for (.*)$")
     public void searchProduct(String product) throws Exception {
+
         page.search(product);
     }
 
     @Then("search should complete")
     public void searchComplete() {
+
         System.out.println("Search Completed");
     }
 
     @When("user adds (.*) to cart$")
     public void addProduct(String product) throws Exception {
+
         page.addToCart(product);
     }
 
     @Then("product should be added successfully")
     public void productAdded() {
+
         System.out.println("Product Added Successfully");
     }
 
     @When("user removes products from cart")
     public void removeProducts() throws Exception {
+
         page.removeToCart();
     }
 
     @Then("cart should update")
     public void cartUpdated() {
+
         System.out.println("Cart Updated");
     }
 
     @When("user proceeds to checkout")
     public void checkoutProduct() {
+
         page.checkout();
     }
 
     @Then("checkout page should open")
     public void checkoutSuccess() {
+
         System.out.println("Checkout Page Opened");
     }
 
     @When("user logs out")
     public void logoutUser() throws Exception {
+
         page.logoutUser();
     }
 
     @Then("logout should be successful")
     public void logoutSuccess() {
+
         System.out.println("Logout Successful");
     }
 }
