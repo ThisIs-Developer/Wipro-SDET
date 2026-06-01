7e1b01ca5b61ab3767c4b4afae2a9e735d33b430
Baivab Sarkar <baivabsarkar@gmail.com>
2026-06-01 17:49:35 +0530

    Fixed Cucumber TestNG Jenkins execution

--- ProjectTutorialsNinjaBBD/src/test/java/hooks/Hooks.java
+++ ProjectTutorialsNinjaBBD/src/test/java/hooks/Hooks.java
@@ -1,25 +1,32 @@
 package hooks;
 
-import io.cucumber.java.AfterAll;
-import io.cucumber.java.BeforeAll;
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.time.Duration;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Hooks {
-    
+
     public static WebDriver driver;
 
-    @BeforeAll
-    public static void setup() {
+    @Before
+    public void setup() {
+
+        WebDriverManager.chromedriver().setup();
+
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
     }
 
-    @AfterAll
-    public static void tearDown() {
+    @After
+    public void tearDown() {
+
         if (driver != null) {
             driver.quit();
         }
--- ProjectTutorialsNinjaBBD/src/test/java/runner/TestRunner.java
+++ ProjectTutorialsNinjaBBD/src/test/java/runner/TestRunner.java
@@ -4,13 +4,13 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-	features = "src/test/resources/features",
-	glue = {"stepDefinitions", "hooks"},
-	plugin = {
-	"pretty",
-	"html:target/htmlreports/report.html",
-	"json:target/jsonreport/report.json"
-	}
+        features = "src/test/resources/features",
+        glue = {"stepDefinitions", "hooks"},
+        plugin = {
+                "pretty",
+                "html:target/htmlreports/report.html",
+                "json:target/jsonreport/report.json"
+        }
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
 
