92399589602f6bc43db0caec849c80b3b3c92526
Baivab Sarkar <baivabsarkar@gmail.com>
2026-06-09 11:00:31 +0530

    Update BaseTest for Docker Run

--- BlazeDemo/src/test/java/base/BaseTest.java
+++ BlazeDemo/src/test/java/base/BaseTest.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -24,7 +25,16 @@ public class BaseTest {
 
         switch (browser.toLowerCase()) {
         case "chrome":
-        	driver = new ChromeDriver();
+        	ChromeOptions options = new ChromeOptions();
+
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--remote-allow-origins=*");
+
+            driver = new ChromeDriver(options);
+
             Log.info("Launching Browser : " + browser);
             break;
 
